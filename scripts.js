1c3e073aa06c0e23308770afdeb51aa2b9f74996
Jasleen Kaur <kaurjasleen1728@gmail.com>
2025-01-10 00:06:54 +0530

    added typewriter effect link

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@ function generateQuote() {
   let apiKey, prompt, context, apiUrl;
   apiKey = `2d8o4b96bdta6ee065c85fc43853285d`;
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={key}`;
+
+  let newQuote = document.querySelector(".quote-section");
+  newQuote.innerHTML=`{response.data.answer}`;
 }
 
 let motivateButtonElement = document.querySelector("button");
